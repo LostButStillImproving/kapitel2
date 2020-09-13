1c8fa3e60a1694541e69810e662ca33033b5d4f1
LostButStillImproving <mike6353@edu.easj.dk>
2020-09-13 15:47:49 +0200

    Initial commit

---
+++ src/kapitel2.java
@@ -0,0 +1,121 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.lang.Math.*;
+import static java.lang.String.*;
+
+public class kapitel2 {
+
+    public static class opgave1 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.print("Enter miles: ");
+            int number = Integer.valueOf(scan.nextLine());
+            System.out.println(number + " miles is " + (Math.round((number * 1.6) * 10) / 10.0) + " kilometers");
+        }
+    }
+
+    public static class opgave2 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Enter the length of the sides and height of the Equilateral triangle:");
+
+            double length = Double.parseDouble(scan.nextLine());
+            double area = ((sqrt(3) / 4) * pow(length, 2));
+            System.out.println("The area is " + roundto2(area));
+            double volume = area * length;
+            System.out.println("The volume of the Triangular prism is " + roundto2(volume));
+        }
+
+    }
+
+    public static class opgave3 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Enter a value for meter:");
+            double value = Double.parseDouble(scan.nextLine());
+            System.out.println(value + " meters is " + roundto2(value * 3.2786) + " feet");
+        }
+    }
+
+    public static class opgave4 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Enter a number in square meters:");
+            double sqm = Double.parseDouble(scan.nextLine());
+            System.out.println(sqm + " square meters is " + roundto2(sqm * 0.3025) + "pings");
+
+        }
+    }
+
+    public static class opgave5 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Enter the subtotal and gratuity rate:");
+            String subandgratuity = scan.nextLine();
+            String[] subandgrat = (subandgratuity.split(" "));
+
+            double subtotal = Integer.parseInt(subandgrat[0]);
+            double gratuityrate = Integer.parseInt(subandgrat[1]);
+
+            double gratuity = subtotal * gratuityrate / 100.0;
+            double total = subtotal + gratuity;
+
+
+            System.out.println("The gratuity is " + gratuity + " and total is " + total);
+
+        }
+    }
+
+    public static class opgave6 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Enter a number between 0 and 1000:");
+            int number = Integer.valueOf(scan.nextLine());
+            int number1 = number;
+
+            if (number < 1000 && number > 0) {
+                List<Integer> digits = new ArrayList<>();
+
+                while ((int) (Math.log10(number) + 1) >= 1) {
+
+                    digits.add(number % 10);
+                    number = number / 10;
+                }
+
+                int result = 1;
+                for (int i : digits) {
+                    result *= i;
+                }
+                System.out.println("The multiplication of all digits in " + number1 + " is " + result);
+
+            } else {
+                System.out.println("something wrong with your number, but I'm not gonna tell ya what");
+            }
+        }
+    }
+
+    public static class opgave7 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter the number of minutes:");
+            int minutes = Integer.valueOf(scan.nextLine());
+            double hours = minutes/60;
+            double days = hours/24;
+            int years = (int) days/365;
+            double remaingDays = days % 365;
+            System.out.println(minutes + " minutes is approximately " + years + " and " + (int)remaingDays + " days");
+        }
+    }
+
+    public static double roundto2(double number) {
+        return Math.round(number * 100) / 100.0;
+    }
+}
---
+++ tests/kapitel2Test.java
@@ -0,0 +1,40 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import static org.junit.Assert.assertEquals;
+
+public class kapitel2Test {
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    public void opgave1test(){
+
+        kapitel2.opgave1 opgave1 = new kapitel2.opgave1();
+
+        assertEquals(5, outContent.toString().strip());
+
+
+
+    }
+
+}
